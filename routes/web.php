30fad385afde797809745292047a241bac9af2a6
Gadurp1 <drexelunderwood@Drexels-MacBook-Pro.local>
2016-11-16 09:19:25 -0600

    Initial Commit

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+use Vinkla\Instagram\Instagram;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    $instagram = new Instagram();
+    $instagram = $instagram->get('getrealclothing');
+    $posts=TCG\Voyager\Models\Post::get();
+    return view('welcome',compact('posts','instagram'));
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+Route::get('{slug}', function ($slug) {
+
+    $post=TCG\Voyager\Models\Post::where('slug',$slug)->first();
+    return view('show',compact('post'));
+
+});
